e6876a211e16771674ef193593600fdbfb21112a
Gill <71924114+asterismagic@users.noreply.github.com>
2024-07-12 20:38:32 +0800

    full card deck, partial image links

--- lib/tarotcontent.dart
+++ lib/tarotcontent.dart
@@ -1,18 +1,181 @@
 const List tarotdeck = [
   {
-    'title':"Fool – 0",
-    "image":"",
-    "meaning":"Fool Meaning"
-  },{
-    'title':"Fool Reversed– 0",
-    "meaning":"Fool Reversed Meaning"
+    'title': "Fool – 0",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/d/d9/Pictorial_Key_to_the_Tarot_00_The_Fool.jpg",
+    "meaning": "Fool Meaning"
   },
   {
-    'title':"Magician – I",
-    "meaning":"Magician Meaning"
+    'title': "Fool Reversed– 0",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/d/d9/Pictorial_Key_to_the_Tarot_00_The_Fool.jpg",
+    "meaning": "Fool Reversed Meaning"
   },
   {
-    'title':"Magician Reversed – I",
-    "meaning":"Magician Reversed Meaning"
-  }
+    'title': "Magician – I",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/6/62/Pictorial_Key_to_the_Tarot_01_The_Magician.jpg",
+    "meaning": "Magician Meaning"
+  },
+  {
+    'title': "Magician Reversed – I",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/6/62/Pictorial_Key_to_the_Tarot_01_The_Magician.jpg",
+    "meaning": "Magician Reversed Meaning"
+  },
+  {
+    'title': "High Priestess – II",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/7/7e/Pictorial_Key_to_the_Tarot_02_The_High_Priestess.jpg",
+    "meaning": "High Priestess Meaning"
+  },
+  {
+    'title': "High Priestess Reversed – II",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/7/7e/Pictorial_Key_to_the_Tarot_02_The_High_Priestess.jpg",
+    "meaning": "High Priestess Reversed Meaning"
+  },
+  {
+    'title': "Empress – III",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/e/ef/Pictorial_Key_to_the_Tarot_03_The_Empress.jpg",
+    "meaning": "Empress Meaning"
+  },
+  {
+    'title': "Empress Reversed – III",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/e/ef/Pictorial_Key_to_the_Tarot_03_The_Empress.jpg",
+    "meaning": "Empress Reversed Meaning"
+  },
+  {
+    'title': "Emperor – IV",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/9/92/Pictorial_Key_to_the_Tarot_04_The_Emperor.jpg",
+    "meaning": "Emperor Meaning"
+  },
+  {
+    'title': "Emperor Reversed – IV",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/9/92/Pictorial_Key_to_the_Tarot_04_The_Emperor.jpg",
+    "meaning": "Emperor Reversed Meaning"
+  },
+  {
+    'title': "Hierophant – V",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/e/e3/Pictorial_Key_to_the_Tarot_05_The_Hierophant.jpg",
+    "meaning": "Hierophant Meaning"
+  },
+  {
+    'title': "Hierophant Reversed – V",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/e/e3/Pictorial_Key_to_the_Tarot_05_The_Hierophant.jpg",
+    "meaning": "Hierophant Reversed Meaning"
+  },
+  {
+    'title': "Lovers – VI",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/e/e1/Pictorial_Key_to_the_Tarot_06_The_Lovers.jpg",
+    "meaning": "Lovers Meaning"
+  },
+  {
+    'title': "Lovers Reversed – VI",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/e/e1/Pictorial_Key_to_the_Tarot_06_The_Lovers.jpg",
+    "meaning": "Lovers Reversed Meaning"
+  },
+  {
+    'title': "Chariot – VII",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/4/4a/Pictorial_Key_to_the_Tarot_07_The_Chariot.jpg",
+    "meaning": "Chariot Meaning"
+  },
+  {
+    'title': "Chariot Reversed – VII",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/4/4a/Pictorial_Key_to_the_Tarot_07_The_Chariot.jpg",
+    "meaning": "Chariot Reversed Meaning"
+  },
+  {
+    'title': "Strength – VIII",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/2/2b/Pictorial_Key_to_the_Tarot_08_Strength.jpg",
+    "meaning": "Strength Meaning"
+  },
+  {
+    'title': "Strength Reversed – VIII",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/2/2b/Pictorial_Key_to_the_Tarot_08_Strength.jpg",
+    "meaning": "Strength Reversed Meaning"
+  },
+  {
+    'title': "Hermit – IX",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/6/61/Pictorial_Key_to_the_Tarot_09_The_Hermit.jpg",
+    "meaning": "Hermit Meaning"
+  },
+  {
+    'title': "Hermit Reversed – IX",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/6/61/Pictorial_Key_to_the_Tarot_09_The_Hermit.jpg",
+    "meaning": "Hermit Reversed Meaning"
+  },
+  {
+    'title': "Wheel of Fortune – X",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/8/81/Pictorial_Key_to_the_Tarot_10_Wheel_of_Fortune.jpg",
+    "meaning": "Wheel of Fortune Meaning"
+  },
+  {
+    'title': "Wheel of Fortune Reversed – X",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/8/81/Pictorial_Key_to_the_Tarot_10_Wheel_of_Fortune.jpg",
+    "meaning": "Wheel of Fortune Reversed Meaning"
+  },
+  {
+    'title': "Justice – XI",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/8/84/Pictorial_Key_to_the_Tarot_11_Justice.jpg",
+    "meaning": "Justice Meaning"
+  },
+  {
+    'title': "Justice Reversed – XI",
+    "image":
+        "https://upload.wikimedia.org/wikipedia/commons/8/84/Pictorial_Key_to_the_Tarot_11_Justice.jpg",
+    "meaning": "Justice Reversed Meaning"
+  },
+  {'title': "The Hanged Man – XII", "meaning": "The Hanged Man Meaning"},
+  {
+    'title': "The Hanged Man Reversed – XII",
+    "meaning": "The Hanged Man Reversed Meaning"
+  },
+  {'title': "Death – XIII", "meaning": "Death Meaning"},
+  {'title': "Death Reversed – XIII", "meaning": "Death Reversed Meaning"},
+  {'title': "Temperance – XIV", "meaning": "Temperance Meaning"},
+  {
+    'title': "Temperance Reversed – XIV",
+    "meaning": "Temperance Reversed Meaning"
+  },
+  {'title': "Devil – XV", "meaning": "Devil Meaning"},
+  {'title': "Devil Reversed – XV", "meaning": "Devil Reversed Meaning"},
+  {'title': "The Tower  – XVI", "meaning": "The Tower Meaning"},
+  {
+    'title': "The Tower Reversed – XVI",
+    "meaning": "The Tower Reversed Meaning"
+  },
+  {'title': "The Star – XVII", "meaning": "The Star Meaning"},
+  {'title': "The Star Reversed – XVII", "meaning": "The Star Reversed Meaning"},
+  {'title': "The Moon – XVIII", "meaning": "The Moon Meaning"},
+  {
+    'title': "The Moon Reversed – XVIII",
+    "meaning": "The Moon Reversed Meaning"
+  },
+  {'title': "The Sun – XIX", "meaning": "The Sun Meaning"},
+  {'title': "The Sun Reversed – XIX", "meaning": "The Sun Reversed Meaning"},
+  {'title': "Judgement – XX", "meaning": "Judgement Meaning"},
+  {'title': "Judgement Reversed – XX", "meaning": "Judgement Reversed Meaning"},
+  {'title': "The World – XXI", "meaning": "The World Meaning"},
+  {
+    'title': "The World Reversed – XXI",
+    "meaning": "The World Reversed Meaning"
+  },
 ];
